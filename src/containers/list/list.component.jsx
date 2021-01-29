eb3b2351ac958c654e984acdada69fada4410c5a
Alan <alan_toro@hotmail.com>
2021-01-29 10:29:02 -0300

    remove mock data, set state from prefetch

--- src/containers/list/list.component.jsx
+++ src/containers/list/list.component.jsx
@@ -1,76 +1,42 @@
 import React from 'react';
-import BoxContainer from '../../components/boxContainer'
-import BreadCrumb from '../../components/breadCrumb'
-import ProductList from '../../components/prductList'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+import BoxContainer from '../../components/boxContainer';
+import BreadCrumb from '../../components/breadCrumb';
+import ProductList from '../../components/prductList';
+
+import {list} from '../../utils/mocks';
 
 export class List extends React.Component {
-    constructor() {
+    constructor(props) {
         console.log( 'list.constructor()' );
         super();
-        this.state = {
-            query: {  
-                "author": {    
-                    "name": 'Alan',
-                    "lastname": 'Toro'  
-                },
-                categories: ['MLA81636', 'MLA32277', 'MLA24681'],  
-                items: [    
-                    {      
-                        "id": 'MLA876641593',      
-                        "title": 'Fuente Agua Recirculante Acero Inox Bebedero 2.4l Aurelia',      
-                        "price": {        
-                            "currency": 'ARS',       
-                            "amount": 4690,        
-                            "decimals": null      
-                        },      
-                        "picture": 'http://http2.mlstatic.com/D_661276-MLA32959192845_112019-O.jpg',      
-                        "condition": 'new',      
-                        "city": 'Capital Federal',
-                        "free_shipping": true    
-                    },
-                    {      
-                        "id": 'MLA826741593',      
-                        "title": 'Fuente AguaSape',      
-                        "price": {        
-                            "currency": 'ARS',       
-                            "amount": 4690,        
-                            "decimals": null      
-                        },      
-                        "picture": 'http://http2.mlstatic.com/D_661276-MLA32959192845_112019-O.jpg',      
-                        "condition": 'new',      
-                        "city": 'Capital Federal',
-                        "free_shipping": false    
-                    },
-                    {      
-                        "id": 'MLA826661593',      
-                        "title": 'Fuente de agua de rockencio',      
-                        "price": {        
-                            "currency": 'ARS',       
-                            "amount": 4690,        
-                            "decimals": null      
-                        },      
-                        "picture": 'http://http2.mlstatic.com/D_661276-MLA32959192845_112019-O.jpg',      
-                        "condition": 'new',      
-                        "city": 'Mendoza',
-                        "free_shipping": true    
-                    },
-                ]
-            },
-            categories: [
-                        {
-                          "id": "MLA1071",
-                          "name": "Animales y Mascotas"
-                        },
-                        {
-                          "id": "MLA1081",
-                          "name": "Gatos"
-                        },
-                        {
-                          "id": "MLA81636",
-                          "name": "Comederos y Bebederos"
-                        }
-              ], 
+        if( props.staticContext ) {
+            this.state = {
+                isLoading: false,
+                items: props.stataicContext.list
+            };
+        } else if( window.initial_state ) {
+            this.state = {
+                isLoading: false,
+                items: props.stataicContext.list
+            };
+        } else {
+            this.state = {
+                isLoading: true,
+                items: ''
+            };
         }
+    }
+    static fetchData() {
+
+        return axios.get( 'https://jsonplaceholder.typicode.com/posts/3' ).then( response => {
+            return {
+                title: response.data.title,
+                body: response.data.body,
+            };
+        });
     }
     render() {
         console.log( 'list.render()' );
@@ -79,7 +45,7 @@ export class List extends React.Component {
             <div className='container'>
                 <BreadCrumb/>
                 <BoxContainer>
-                    <ProductList items={this.state.query.items} />
+                    <ProductList items={this.state.items} />
                 </BoxContainer>
             </div>
         );
